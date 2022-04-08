e4dad66992d38cd478677f756506ab1f1e21d4bd
Mirza <herendamirza87@gmail.com>
2022-04-08 10:57:42 +0200

    another change

--- ex12/src/index.js
+++ ex12/src/index.js
@@ -1,13 +1,8 @@
 function splitArrayInGroups(arr, n) {
 
     var result = [];
-    for (var i = 0; i <= arr.length; i++) {
-        result.push(arr.splice(0,n));
-        n-3;
-    }
-
-    if (arr.length) {
-        result.push(arr);
+    for (var i = 0; i <n;i++) {
+        result.push(arr.slice(i*n,(i+1)*n));
     }
     return result;
 }
@@ -20,3 +15,4 @@ console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
 console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5], 2));
 console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 3));
 console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
+module.exports = splitArrayInGroups;
